d3ae0082f143705101a77ce6323c3c7a696c8f43
chriswait <2563474+chriswait@users.noreply.github.com>
2021-03-17 18:24:55 +0000

    adds rating radio buttons

--- resources/views/my-journey.blade.php
+++ resources/views/my-journey.blade.php
@@ -15,28 +15,51 @@
     <thead>
         <tr>
             <th>1. Confidence</th>
-            <th>Nervous</th>
-            <th></th>
-            <th>Confident</th>
+            <th>Very Difficult</th>
+            <th>Difficult</th>
+            <th>Not Sure</th>
+            <th>Easy</th>
+            <th>Very Easy</th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td>How confident do you feel in general?</td>
-            <td><input type='radio' name='general' value='1'></td>
-            <td><input type='radio' name='general' value='2'></td>
-            <td><input type='radio' name='general' value='3'></td>
-        </tr>
-        <tr>
-            <td>How do you feel about trying new things?</td>
-            <td><input type='radio' name='new-things' value='1'></td>
-            <td><input type='radio' name='new-things' value='2'></td>
-            <td><input type='radio' name='new-things' value='3'></td>
+            <td>
+                <label>
+                    <input type='radio' name='general' value='1'>
+                    <img src='img/rating-1.svg' />
+                </label>
+            </td>
+            <td>
+                <label>
+                    <input type='radio' name='general' value='2'>
+                    <img src='img/rating-2.svg' />
+                </label>
+            </td>
+            <td>
+                <label>
+                    <input type='radio' name='general' value='3'>
+                    <img src='img/rating-3.svg' />
+                </label>
+            </td>
+            <td>
+                <label>
+                    <input type='radio' name='general' value='4'>
+                    <img src='img/rating-4.svg' />
+                </label>
+            </td>
+            <td>
+                <label>
+                    <input type='radio' name='general' value='5'>
+                    <img src='img/rating-5.svg' />
+                </label>
+            </td>
         </tr>
     </tbody>
     <tfoot>
         <tr>
-            <td colspan='4'>
+            <td colspan='6'>
                 <p>
                 Do you have any extra comments or pictures you would like to share on the topic of confidence?
                 </p>
