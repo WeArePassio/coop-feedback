7b02ded349371ea356fa2ffdf9a265f2825eac27
chriswait <2563474+chriswait@users.noreply.github.com>
2021-03-17 23:25:49 +0000

    prev button, move repsonses into provider, text

--- resources/src/feedback-form/MyJourney.js
+++ resources/src/feedback-form/MyJourney.js
@@ -6,13 +6,23 @@ import {useQuestions} from './QuestionsProvider';
 
 const MyJourney = () => {
   const [themeIndex, setThemeIndex] = useState(0);
-  const {questionThemes, fetchQuestions} = useQuestions();
-  const [responses, setResponses] = useState();
+  const {questionThemes, fetchQuestions, responses, setResponse} = useQuestions();
+
   useEffect(() => {
     fetchQuestions();
   }, []);
+
   const theme = questionThemes ? questionThemes[themeIndex] : undefined;
   const questions = theme ? theme.questions : [];
+
+  const handleNext = () => {
+    if (themeIndex < questionThemes.length - 1) {
+      setThemeIndex(themeIndex + 1);
+    } else {
+      console.log(responses);
+    }
+  };
+
   return (
     <>
       <img src={start} className='flag' alt='A flag with the word Start' />
@@ -28,53 +38,59 @@ const MyJourney = () => {
       </div>
 
       {theme && (
-        <table className='rating-table'>
-          <thead>
-            <tr>
-              <th>{`${themeIndex + 1}. ${theme.title}`}</th>
-              <th>Very Difficult</th>
-              <th>Difficult</th>
-              <th>Not Sure</th>
-              <th>Easy</th>
-              <th>Very Easy</th>
-            </tr>
-          </thead>
-          <tbody>
-            {questions &&
-              questions.map(({id, title}) => (
-                <RatingRow
-                  key={id}
-                  question={title}
-                  question_id={`question-${id}`}
-                  value={responses?.[theme.id]?.[id]}
-                  setValue={(value) => {
-                    const newResponses = {...responses};
-                    if (!Object.keys(newResponses).includes(`${theme.id}`)) {
-                      newResponses[theme.id] = {};
-                    }
-                    newResponses[theme.id][id] = value;
-                    setResponses(newResponses);
-                  }}
-                />
-              ))}
-          </tbody>
-        </table>
+        <>
+          <table className='rating-table'>
+            <thead>
+              <tr>
+                <th>{`${themeIndex + 1}. ${theme.title}`}</th>
+                <th>Very Difficult</th>
+                <th>Difficult</th>
+                <th>Not Sure</th>
+                <th>Easy</th>
+                <th>Very Easy</th>
+              </tr>
+            </thead>
+            <tbody>
+              {questions &&
+                questions.map(({id, title}) => (
+                  <RatingRow
+                    key={id}
+                    question={title}
+                    question_id={`question-${id}`}
+                    value={responses?.[theme.id]?.[id]}
+                    setValue={(value) => {
+                      setResponse(theme.id, id, value);
+                    }}
+                  />
+                ))}
+            </tbody>
+          </table>
+          <div className='panel'>
+            <p>
+              {`Do you have any extra comments or pictures you would like to share on the topic of
+              ${theme.title}?`}
+            </p>
+            <textarea
+              placeholder='Type here...'
+              value={responses?.[theme.id]?.text ?? ''}
+              onChange={(event) => {
+                setResponse(theme.id, 'text', event.target.value);
+              }}></textarea>
+          </div>
+        </>
       )}
-      <div className='panel'>
-        <p>
-          Do you have any extra comments or pictures you would like to share on the topic of
-          confidence?
-        </p>
-        <textarea placeholder='Type here...'></textarea>
-      </div>
 
       <div className='button-row'>
-        <button
-          onClick={() => {
-            setThemeIndex(themeIndex + 1);
-          }}>
-          Next
-        </button>
+        {themeIndex > 0 && (
+          <button
+            style={{marginRight: 'auto'}}
+            onClick={() => {
+              setThemeIndex(themeIndex - 1);
+            }}>
+            Previous
+          </button>
+        )}
+        <button onClick={handleNext}>Next</button>
       </div>
     </>
   );
