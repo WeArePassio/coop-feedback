38258297a84dfabc5544969541b78ced5ba3abd5
chriswait <2563474+chriswait@users.noreply.github.com>
2021-03-24 08:47:40 +0000

    add dot to "complete" panel

--- resources/src/feedback-form/Complete.js
+++ resources/src/feedback-form/Complete.js
@@ -8,11 +8,13 @@ const Complete = () => {
         <div className='line'></div>
       </header>
       <div className='panel'>
-        <p>
-          Thank you for taking the time to answer these questions. We will ask you to rate these
-          topics again at the end of your journey so you can see how the course has helped you to
-          improve. We hope you enjoy your time with the Co-operative College!
-        </p>
+        <section>
+          <p>
+            Thank you for taking the time to answer these questions. We will ask you to rate these
+            topics again at the end of your journey so you can see how the course has helped you to
+            improve. We hope you enjoy your time with the Co-operative College!
+          </p>
+        </section>
       </div>
     </>
   );
