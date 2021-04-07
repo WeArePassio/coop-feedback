56623c4ff2ca3cb75558bbe41ee5d7dd32d1a783
chriswait <2563474+chriswait@users.noreply.github.com>
2021-04-07 13:10:13 +0100

    empty states on submission screens

--- resources/src/feedback-form/Project/Submissions.js
+++ resources/src/feedback-form/Project/Submissions.js
@@ -16,66 +16,70 @@ const Submissions = () => {
   return (
     <>
       <h1>Submissions</h1>
-      {submissions.map(
-        (
-          {
-            created_at,
-            name,
-            submission_type,
-            submission,
-            project_feedback_ratings,
-            project_feedback_comments,
-          },
-          index
-        ) => (
-          <div key={`submission-${index}`}>
-            <h2>
-              {name} ({new Date(created_at).toDateString()})
-            </h2>
-            {submission_type === `App\\Models\\BeginningFeedbackSubmission` ? (
-              <>
-                <label>Who am I?</label>
-                <p>{submission.who_am_i}</p>
-                <label>Why am I Here?</label>
-                <p>{submission.why_am_i_here}</p>
-              </>
-            ) : (
-              <>
-                <label>How to Improve Project?</label>
-                <p>{submission.improve_project}</p>
-                <label>Favourite Activities</label>
-                <p>{submission.favourite_activities}</p>
-              </>
-            )}
-            {questionThemes.map((theme, themeIndex) => (
-              <div key={`theme-${themeIndex}`}>
-                <h3>{theme.title}</h3>
-                <table className='rating-table submission-table'>
-                  <tbody>
-                    {theme.questions.map((question, questionIndex) => (
-                      <tr key={`question-${questionIndex}`}>
-                        <td>{question.title}</td>
-                        <td>
-                          {project_feedback_ratings.find(
-                            (rating) => rating.question_id === question.id
-                          )?.rating ?? ''}
-                          /5
+      {submissions.length === 0 ? (
+        <h2>No Submissions</h2>
+      ) : (
+        submissions.map(
+          (
+            {
+              created_at,
+              name,
+              submission_type,
+              submission,
+              project_feedback_ratings,
+              project_feedback_comments,
+            },
+            index
+          ) => (
+            <div key={`submission-${index}`}>
+              <h2>
+                {name} ({new Date(created_at).toDateString()})
+              </h2>
+              {submission_type === `App\\Models\\BeginningFeedbackSubmission` ? (
+                <>
+                  <label>Who am I?</label>
+                  <p>{submission.who_am_i}</p>
+                  <label>Why am I Here?</label>
+                  <p>{submission.why_am_i_here}</p>
+                </>
+              ) : (
+                <>
+                  <label>How to Improve Project?</label>
+                  <p>{submission.improve_project}</p>
+                  <label>Favourite Activities</label>
+                  <p>{submission.favourite_activities}</p>
+                </>
+              )}
+              {questionThemes.map((theme, themeIndex) => (
+                <div key={`theme-${themeIndex}`}>
+                  <h3>{theme.title}</h3>
+                  <table className='rating-table submission-table'>
+                    <tbody>
+                      {theme.questions.map((question, questionIndex) => (
+                        <tr key={`question-${questionIndex}`}>
+                          <td>{question.title}</td>
+                          <td>
+                            {project_feedback_ratings.find(
+                              (rating) => rating.question_id === question.id
+                            )?.rating ?? ''}
+                            /5
+                          </td>
+                        </tr>
+                      ))}
+                      <tr>
+                        <td colSpan={2}>
+                          <span style={{fontWeight: 'bold'}}>Comments: </span>
+                          {project_feedback_comments.find(
+                            (comment) => comment.question_theme_id === theme.id
+                          )?.text ?? ''}
                         </td>
                       </tr>
-                    ))}
-                    <tr>
-                      <td colSpan={2}>
-                        <span style={{fontWeight: 'bold'}}>Comments: </span>
-                        {project_feedback_comments.find(
-                          (comment) => comment.question_theme_id === theme.id
-                        )?.text ?? ''}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            ))}
-          </div>
+                    </tbody>
+                  </table>
+                </div>
+              ))}
+            </div>
+          )
         )
       )}
     </>
--- resources/src/feedback-form/Session/Submissions.js
+++ resources/src/feedback-form/Session/Submissions.js
@@ -15,41 +15,45 @@ const Submissions = () => {
   return (
     <>
       <h1>Submissions</h1>
-      {submissions.map(
-        (
-          {
-            created_at,
-            enjoyment_rating,
-            enjoyed_most,
-            democracy,
-            self_help,
-            self_responsibility,
-            equality,
-            equity,
-            solidarity,
-            openness,
-            honesty,
-            social_responsibility,
-          },
-          index
-        ) => (
-          <div key={`submission-${index}`} style={{marginBottom: 20}}>
-            <div>Created at: {created_at}</div>
-            <div>Enjoyment Rating: {enjoyment_rating}</div>
-            <div>Enjoyed Most: {enjoyed_most}</div>
-            <div>
-              Values:
-              <div>democracy: {democracy}</div>
-              <div>self_help: {self_help}</div>
-              <div>self_responsibility: {self_responsibility}</div>
-              <div>equality: {equality}</div>
-              <div>equity: {equity}</div>
-              <div>solidarity: {solidarity}</div>
-              <div>openness: {openness}</div>
-              <div>honesty: {honesty}</div>
-              <div>social_responsibility: {social_responsibility}</div>
+      {submissions.length === 0 ? (
+        <h2>No Submissions</h2>
+      ) : (
+        submissions.map(
+          (
+            {
+              created_at,
+              enjoyment_rating,
+              enjoyed_most,
+              democracy,
+              self_help,
+              self_responsibility,
+              equality,
+              equity,
+              solidarity,
+              openness,
+              honesty,
+              social_responsibility,
+            },
+            index
+          ) => (
+            <div key={`submission-${index}`} style={{marginBottom: 20}}>
+              <div>Created at: {created_at}</div>
+              <div>Enjoyment Rating: {enjoyment_rating}</div>
+              <div>Enjoyed Most: {enjoyed_most}</div>
+              <div>
+                Values:
+                <div>democracy: {democracy}</div>
+                <div>self_help: {self_help}</div>
+                <div>self_responsibility: {self_responsibility}</div>
+                <div>equality: {equality}</div>
+                <div>equity: {equity}</div>
+                <div>solidarity: {solidarity}</div>
+                <div>openness: {openness}</div>
+                <div>honesty: {honesty}</div>
+                <div>social_responsibility: {social_responsibility}</div>
+              </div>
             </div>
-          </div>
+          )
         )
       )}
     </>
