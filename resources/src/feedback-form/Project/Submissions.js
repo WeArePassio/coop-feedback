fa9cb5f7132b4d23cfb9188c0ab6221d99b96d86
chriswait <2563474+chriswait@users.noreply.github.com>
2021-04-14 18:44:59 +0100

    text responses + images, first pass

--- resources/src/feedback-form/Project/Submissions.js
+++ resources/src/feedback-form/Project/Submissions.js
@@ -2,6 +2,8 @@ import React, {useEffect} from 'react';
 
 import {useProject} from './ProjectProvider';
 
+import SubmissionHeader from '../SubmissionHeader';
+
 const Submissions = () => {
   const {submissions, questionThemes, fetchQuestions, fetchSubmissions} = useProject();
 
@@ -13,83 +15,79 @@ const Submissions = () => {
     init();
   }, []);
 
+  const whoAmIResponses = [];
+  const whyAmIHereResponses = [];
+  const aboutMeImages = [];
+  const improveProjectResponses = [];
+  const favouriteActivitiesResponses = [];
+  submissions.forEach((submission) => {
+    if (submission.submission_type === `App\\Models\\BeginningFeedbackSubmission`) {
+      whoAmIResponses.push(submission.submission.who_am_i);
+      whyAmIHereResponses.push(submission.submission.why_am_i_here);
+      if (submission.submission.image) {
+        aboutMeImages.push(submission.submission.image);
+      }
+    } else {
+      improveProjectResponses.push(submission.submission.improve_project);
+      favouriteActivitiesResponses.push(submission.submission.favourite_activities);
+    }
+  });
+
   return (
     <>
       <h1>Submissions</h1>
       {submissions.length === 0 ? (
         <h2>No Submissions</h2>
       ) : (
-        submissions.map(
-          (
-            {
-              created_at,
-              name,
-              submission_type,
-              submission,
-              project_feedback_ratings,
-              project_feedback_comments,
-            },
-            index
-          ) => (
-            <div key={`submission-${index}`}>
-              <h2>
-                {name} ({new Date(created_at).toDateString()})
-              </h2>
-              {submission_type === `App\\Models\\BeginningFeedbackSubmission` ? (
-                <>
-                  <label>Who am I?</label>
-                  <p>{submission.who_am_i}</p>
-                  <label>Why am I Here?</label>
-                  <p>{submission.why_am_i_here}</p>
-                  {submission.image && (
-                    <a href={submission.image} target='_blank' rel='noreferrer'>
-                      <img
-                        src={`${submission.image}`}
-                        alt=''
-                        style={{width: 500, maxWidth: '100%'}}
-                      />
-                    </a>
-                  )}
-                </>
-              ) : (
-                <>
-                  <label>How to Improve Project?</label>
-                  <p>{submission.improve_project}</p>
-                  <label>Favourite Activities</label>
-                  <p>{submission.favourite_activities}</p>
-                </>
-              )}
-              {questionThemes.map((theme, themeIndex) => (
-                <div key={`theme-${themeIndex}`}>
-                  <h3>{theme.title}</h3>
-                  <table className='rating-table submission-table'>
-                    <tbody>
-                      {theme.questions.map((question, questionIndex) => (
-                        <tr key={`question-${questionIndex}`}>
-                          <td>{question.title}</td>
-                          <td>
-                            {project_feedback_ratings.find(
-                              (rating) => rating.question_id === question.id
-                            )?.rating ?? ''}
-                            /5
-                          </td>
-                        </tr>
-                      ))}
-                      <tr>
-                        <td colSpan={2}>
-                          <span style={{fontWeight: 'bold'}}>Comments: </span>
-                          {project_feedback_comments.find(
-                            (comment) => comment.question_theme_id === theme.id
-                          )?.text ?? ''}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+        <>
+          <SubmissionHeader text='[TODO: Project Name]' />
+          <div className='submission-session'>
+            <h3>Who am I? Where am I from? What is important to me?</h3>
+            {whoAmIResponses.map((text, index) => (
+              <div key={index} className='submission-text-response'>
+                {text}
+              </div>
+            ))}
+            <div className='divider' />
+            <h3>Why am I here and what would I like to get from the project?</h3>
+            {whyAmIHereResponses.map((text, index) => (
+              <div key={index} className='submission-text-response'>
+                {text}
+              </div>
+            ))}
+            <div className='divider' />
+            <h3>About Me</h3>
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+              {aboutMeImages.map((image, index) => (
+                <div style={{width: 150}} key={index}>
+                  <img src={image} style={{width: 120, height: 120, backgroundColor: 'white'}} />
+                  <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div>Image {index + 1}</div>
+                    <div>Download</div>
+                  </div>
                 </div>
               ))}
             </div>
-          )
-        )
+            <div className='divider' />
+            <h3>
+              We want to make sure we are running projects which you enjoy and learn from, please
+              write down any comments on how we can improve this project.
+            </h3>
+            {improveProjectResponses.map((text, index) => (
+              <div key={index} className='submission-text-response'>
+                {text}
+              </div>
+            ))}
+            <div className='divider' />
+            <h3>What were your favourite activities on the project?</h3>
+            {favouriteActivitiesResponses.map((text, index) => (
+              <div key={index} className='submission-text-response'>
+                {text}
+              </div>
+            ))}
+            <div className='divider' />
+          </div>
+        </>
       )}
     </>
   );
