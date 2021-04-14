fe76ea5d2a463858d24e914db6e9ef2fc80109a2
chriswait <2563474+chriswait@users.noreply.github.com>
2021-04-14 13:37:37 +0100

    work on rating design

--- resources/src/feedback-form/Session/Submissions.js
+++ resources/src/feedback-form/Session/Submissions.js
@@ -2,6 +2,15 @@ import React, {useEffect, useState} from 'react';
 
 import {useSession, COOP_VALUES} from './SessionProvider';
 
+import rating1 from '../../img/rating-1.svg';
+import rating2 from '../../img/rating-2.svg';
+import rating3 from '../../img/rating-3.svg';
+import rating4 from '../../img/rating-4.svg';
+import rating5 from '../../img/rating-5.svg';
+
+const RATING_LABELS = ['Very unhappy', 'Unhappy', 'Okay', 'Happy', 'Very Happy'];
+const RATING_ICONS = [rating1, rating2, rating3, rating4, rating5];
+
 const formatter = new Intl.DateTimeFormat('en-UK');
 
 function getWeekNumber(d) {
@@ -23,53 +32,25 @@ function getWeekNumber(d) {
   return [d.getUTCFullYear(), weekNo, weekStart];
 }
 
-// const Submission = ({submission}) => {
-//   let {
-//     created_at,
-//     enjoyment_rating,
-//     enjoyed_most,
-//     changes,
-//     other_topics,
-//     democracy,
-//     self_help,
-//     self_responsibility,
-//     equality,
-//     equity,
-//     solidarity,
-//     openness,
-//     honesty,
-//     social_responsibility,
-//   } = submission;
-//   return (
-//     <div style={{marginBottom: 20}}>
-//       {/* <div>Created at: {created_at}</div>
-//       <div>Enjoyment Rating: {enjoyment_rating}</div>
-//       <div>Enjoyed Most: {enjoyed_most}</div>
-//       <div>Changes: {changes}</div>
-//       <div>Other Topics: {other_topics}</div> */}
-//       <div>
-//         {/* Values: */}
-//         <div>democracy: {democracy}</div>
-//         <div>self_help: {self_help}</div>
-//         <div>self_responsibility: {self_responsibility}</div>
-//         <div>equality: {equality}</div>
-//         <div>equity: {equity}</div>
-//         <div>solidarity: {solidarity}</div>
-//         <div>openness: {openness}</div>
-//         <div>honesty: {honesty}</div>
-//         <div>social_responsibility: {social_responsibility}</div>
-//       </div>
-//     </div>
-//   );
-// };
-
 const SessionHeader = ({session}) => {
   const {monday, submissions} = session;
   return (
     <div className='submission-session-header'>
-      <h4>
-        Week {formatter.format(monday)} ({submissions.length} submissions)
-      </h4>
+      Week {formatter.format(monday)} ({submissions.length} submissions)
+    </div>
+  );
+};
+
+const RatingRow = ({index, count}) => {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', marginBottom: 20}} key={index}>
+      <div style={{textAlign: 'center', width: 105}}>
+        <img src={RATING_ICONS[index]} alt='' style={{width: 40, height: 40}} />
+        <div style={{fontSize: '10pt'}}>{RATING_LABELS[index]}</div>
+      </div>
+      <div>
+        {/* TODO - graph */}({count})
+      </div>
     </div>
   );
 };
@@ -100,9 +81,7 @@ const SessionDetails = ({session}) => {
       <div className='submission-session'>
         <p>How much did you enjoy today’s session?</p>
         {ratingCounts.map((count, index) => (
-          <div key={index}>
-            {index} - {count}
-          </div>
+          <RatingRow key={index} index={index} count={count} />
         ))}
         <div className='divider' />
         <p>What did you enjoy most about today's session?</p>
@@ -146,7 +125,7 @@ const Session = ({session, setSelectedSession}) => {
 
   const maxRatingCount = Math.max(...ratingCounts);
   // Reverse ratingCounts so we get the highest rating
-  const maxRating = ratingCounts.reverse().indexOf(maxRatingCount) + 1;
+  const maxRatingIndex = ratingCounts.reverse().indexOf(maxRatingCount);
 
   // Sort the valuesCounts by count, decreasing
   const valuesCountsSorted = Object.entries(valuesCounts).sort(
@@ -159,10 +138,7 @@ const Session = ({session, setSelectedSession}) => {
       <div className='submission-session'>
         <p>How much did you enjoy today’s session?</p>
         <p>Top rated result:</p>
-        <p>
-          {maxRating} ({maxRatingCount})
-        </p>
-        {/* Graph showing number of responses */}
+        <RatingRow index={maxRatingIndex} count={maxRatingCount} />
         <div className='divider' />
         <div>
           <p>What co-operative values did you learn from todays session?</p>
@@ -224,16 +200,14 @@ const Submissions = () => {
 
   return (
     <>
+      {selectedSession && <button onClick={() => setSelectedSession(undefined)}>Back</button>}
       <h1>Submissions</h1>
       {submissions.length === 0 ? (
         <h2>No Submissions</h2>
       ) : (
         <>
           {selectedSession ? (
-            <>
-              <button onClick={() => setSelectedSession(undefined)}>Back</button>
-              <SessionDetails session={selectedSession} />
-            </>
+            <SessionDetails session={selectedSession} />
           ) : (
             sessions.map((session, sessionIndex) => (
               <Session
