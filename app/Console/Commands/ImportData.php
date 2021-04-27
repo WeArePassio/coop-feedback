99e53409befe14ac4706c0c47fd9b23d32ec7cda
chriswait <2563474+chriswait@users.noreply.github.com>
2021-04-27 20:45:48 +0100

    add theme metrics, import them

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Log;
+use App\Models\QuestionThemeMetric;
 use App\Models\QuestionTheme;
 use App\Models\Question;
 
@@ -53,10 +54,52 @@ class ImportData extends Command
      */
     public function handle()
     {
+        $this->importQuestionThemeMetrics();
         $this->importQuestions();
         return 0;
     }
 
+    private function importQuestionThemeMetrics()
+    {
+        $this->log_import('importing metrics...');
+        $dataPath = "question_theme_metrics.csv";
+        $correctNumColumns = 6;
+        $row_index = 1;
+        $path = getcwd() . "/$this->importsDir/$dataPath";
+        if (file_exists($path) && ($handle = fopen($path, "r")) !== false) {
+            while (($row = fgetcsv($handle, 1000, ",")) !== false) {
+                if ($row_index == 1) {
+                    $row_index++;
+                    continue;
+                }
+                // Check we have the right number of columns
+                if (count($row) != $correctNumColumns) {
+                    $this->log_import_error("Incorrect number of columns");
+                    continue;
+                }
+
+                // Check if this stage exists already
+                $name = $row[0];
+                $label1 = $row[1];
+                $label2 = $row[2];
+                $label3 = $row[3];
+                $label4 = $row[4];
+                $label5 = $row[5];
+                $theme = QuestionThemeMetric::updateOrCreate([
+                    'name' => $name,
+                ], [
+                    'label1' => $label1,
+                    'label2' => $label2,
+                    'label3' => $label3,
+                    'label4' => $label4,
+                    'label5' => $label5,
+                ]);
+            }
+            $this->log_import('done!');
+        } else {
+            $this->log_import_error("could not find " . $dataPath);
+        }
+    }
     private function importQuestions()
     {
         $this->log_import('importing questions...');
@@ -79,11 +122,18 @@ class ImportData extends Command
                 // Check if this stage exists already
                 $themeTitle = $row[0];
                 $questionTitle = $row[1];
-                $metric = $row[2];
+                $metric_name = $row[2];
+                $metric = QuestionThemeMetric::where('name', $metric_name)->first();
+                if (!$metric) {
+                    $this->log_import_error("Couldn't find matching metric");
+                    continue;
+                }
                 $theme = QuestionTheme::firstOrCreate([
                     'title' => $themeTitle,
+                ], [
+                    'question_theme_metric_id' => $metric->id,
                 ]);
-                $theme = Question::firstOrCreate([
+                $question = Question::firstOrCreate([
                     'title' => $questionTitle,
                     'question_theme_id' => $theme->id,
                 ]);
