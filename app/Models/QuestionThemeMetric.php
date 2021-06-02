5bdcabebfc2449b11f7b2c1baddc206f28165e55
chriswait <2563474+chriswait@users.noreply.github.com>
2021-06-02 23:19:50 +0100

    update questions & metrics, add heading

--- app/Models/QuestionThemeMetric.php
+++ app/Models/QuestionThemeMetric.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class QuestionThemeMetric extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'label1', 'label2', 'label3', 'label4', 'label5'];
+    protected $fillable = ['name', 'heading', 'label1', 'label2', 'label3', 'label4', 'label5'];
 }
